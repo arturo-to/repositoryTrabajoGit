0cd36ade55c479f0ce1c73b7333a973ed141a8ef
arturo-to <arturotorresorovio@gmail.com>
2023-02-28 20:49:46 +0100

    Creacion nueva clase

---
+++ ProyectoGit/src/paquetetres/Multiplicacion.java
@@ -0,0 +1,21 @@
+package paquetetres;
+
+import paqueteuno.Suma;
+
+public class Multiplicacion {
+	
+	private static int num1 = 5;
+	private static int num2 = 3;
+	
+	public int multiplicar(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	public static void main(String[] args) {
+		Multiplicacion multiplicacion = new Multiplicacion();
+		
+		System.out.println(multiplicacion.multiplicar(num1, num2));
+
+	}
+
+}
