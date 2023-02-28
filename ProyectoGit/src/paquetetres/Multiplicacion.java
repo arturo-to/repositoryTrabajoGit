2719eea923b7c6fe3bb75b32bda5fa411cc95261
arturo-to <arturotorresorovio@gmail.com>
2023-02-28 20:53:57 +0100

    Ramas fusionadas, puedes descargarlo

--- ProyectoGit/src/paquetetres/Multiplicacion.java
+++ ProyectoGit/src/paquetetres/Multiplicacion.java
@@ -12,6 +12,7 @@ public class Multiplicacion {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("He fusionado las ramas");
 		Multiplicacion multiplicacion = new Multiplicacion();
 		
 		System.out.println(multiplicacion.multiplicar(num1, num2));
